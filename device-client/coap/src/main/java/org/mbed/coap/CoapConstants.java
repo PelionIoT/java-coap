fe31f4961988f37ec8680292d9792ab20237d9b4
Ville Oikarinen <ville.oikarinen@arm.com>
2014-08-26 15:27:37 +0300

    more standard order of keywords

--- device-client/coap/src/main/java/org/mbed/coap/CoapConstants.java
+++ device-client/coap/src/main/java/org/mbed/coap/CoapConstants.java
@@ -14,9 +14,9 @@ public final class CoapConstants {
     public static final int DEFAULT_PORT = 5683;
     public static final String WELL_KNOWN_CORE = "/.well-known/core";
     public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
-    public final static long ACK_TIMEOUT = 2000;
-    public final static float ACK_RANDOM_FACTOR = 1.5f;
-    public final static Short MAX_RETRANSMIT = 4;
+    public static final long ACK_TIMEOUT = 2000;
+    public static final float ACK_RANDOM_FACTOR = 1.5f;
+    public static final Short MAX_RETRANSMIT = 4;
 
     private CoapConstants() {
     }
