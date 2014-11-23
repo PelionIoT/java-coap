25aff7e9bb90ab5a8329eabddeacee69dadd5d38
Szymon Sasin <szymon.sasin@arm.com>
2014-11-23 14:03:21 -0500

    removed packetDropping and packetDelay from CoapServer

--- device-client/coap/src/main/java/org/mbed/coap/CoapUtils.java
+++ device-client/coap/src/main/java/org/mbed/coap/CoapUtils.java
@@ -1,12 +1,9 @@
-/**
+/*
  * Copyright (C) 2011-2014 ARM Limited. All rights reserved.
  */
 package org.mbed.coap;
 
 import org.mbed.coap.utils.CoapCallback;
-import java.util.Random;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @author szymon
@@ -46,83 +43,5 @@ public class CoapUtils {
         }
 
         return nullCallback;
-    }
-
-    //TODO: refactor to own class
-    public static interface PacketDropping {    //NOPMD
-
-        boolean drop();
-    }
-
-    //TODO: refactor to own class
-    public static class NoPacketDropping implements PacketDropping {
-
-        @Override
-        public boolean drop() {
-            return false;
-        }
-    }
-
-    //TODO: refactor to own class
-    public static class ProbPacketDropping implements PacketDropping {
-
-        private final byte probability; //0-100
-        private final Random r = new Random();
-
-        public ProbPacketDropping(byte probability) {
-            if (probability < 0 || probability > 100) {
-                throw new IllegalArgumentException("Value must be in range 0-100");
-            }
-            this.probability = probability;
-        }
-
-        @Override
-        public boolean drop() {
-            return (probability <= 0) ? false : (r.nextInt(100) < probability);
-        }
-    }
-
-    //TODO: refactor to own class
-    public static interface PacketDelay {   //NOPMD
-
-        void delay() throws InterruptedException;
-    }
-
-    //TODO: refactor to own class
-    public static class NoPacketDelay implements PacketDelay {
-
-        @Override
-        public void delay() {
-            // nothing to do
-        }
-    }
-
-    //TODO: refactor to own class
-    public static class AvgPacketDelay implements PacketDelay {
-
-        private static final Logger LOGGER = LoggerFactory.getLogger(AvgPacketDelay.class);
-        private final float avgDelay;
-        private final Random random = new Random();
-
-        public AvgPacketDelay(float avgDelay) {
-            this.avgDelay = avgDelay;
-        }
-
-        @Override
-        public void delay() throws InterruptedException {
-            if (this.avgDelay <= 0) {
-                return;
-            }
-
-            double delayTime = avgDelay;
-            delayTime += delayTime * (random.nextGaussian()) / 2;
-            if (delayTime < 0) {
-                delayTime = 0;
-            }
-            if (LOGGER.isDebugEnabled()) {
-                LOGGER.debug("Packet delay = " + delayTime + " ms");
-            }
-            Thread.sleep((long) delayTime);
-        }
     }
 }
