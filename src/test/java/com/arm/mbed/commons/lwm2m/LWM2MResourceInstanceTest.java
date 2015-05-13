27c75f3fee68a5c8f368ff747e79e9a162f440e2
Ville Oikarinen <ville.oikarinen@arm.com>
2015-05-13 15:52:16 +0300

    fixed findbug issues about default encoding
    Now, instead of "s".getBytes() call Utf8Bytes.of("s")

--- src/test/java/com/arm/mbed/commons/lwm2m/LWM2MResourceInstanceTest.java
+++ src/test/java/com/arm/mbed/commons/lwm2m/LWM2MResourceInstanceTest.java
@@ -1,8 +1,7 @@
 package com.arm.mbed.commons.lwm2m;
 
 import static org.junit.Assert.*;
-import com.arm.mbed.commons.lwm2m.LWM2MID;
-import com.arm.mbed.commons.lwm2m.LWM2MResourceInstance;
+import com.arm.mbed.commons.string.Utf8Bytes;
 import org.junit.Test;
 
 public class LWM2MResourceInstanceTest {
@@ -10,77 +9,77 @@ public class LWM2MResourceInstanceTest {
     @Test
     public void createByString() {
         LWM2MResourceInstance instance = new LWM2MResourceInstance(LWM2MID.from(1), "instance");
-        assertEquals(1, instance.getId().intValue() );
-        assertTrue(instance.hasValue() );
-        assertArrayEquals("instance".getBytes(), instance.getValue() );
+        assertEquals(1, instance.getId().intValue());
+        assertTrue(instance.hasValue());
+        assertArrayEquals(Utf8Bytes.of("instance"), instance.getValue());
     }
-    
+
     @Test
     public void createByInteger() throws Exception {
         LWM2MResourceInstance instance = new LWM2MResourceInstance(LWM2MID.from(1), 42);
-        assertEquals(1, instance.getId().intValue() );
-        assertTrue(instance.hasValue() );
-        assertArrayEquals(new byte[] { 42 }, instance.getValue() );
+        assertEquals(1, instance.getId().intValue());
+        assertTrue(instance.hasValue());
+        assertArrayEquals(new byte[]{42}, instance.getValue());
     }
-    
+
     @Test
     public void createBy2ByteInteger() throws Exception {
         LWM2MResourceInstance instance = new LWM2MResourceInstance(LWM2MID.from(1), 554);
-        assertEquals(1, instance.getId().intValue() );
-        assertTrue(instance.hasValue() );
-        assertEquals ("554", instance.getStringValue() );
-        assertArrayEquals(new byte[] { 2, 42 }, instance.getValue() );
+        assertEquals(1, instance.getId().intValue());
+        assertTrue(instance.hasValue());
+        assertEquals("554", instance.getStringValue());
+        assertArrayEquals(new byte[]{2, 42}, instance.getValue());
     }
-    
+
     @Test
     public void createBy3ByteInteger() throws Exception {
         LWM2MResourceInstance instance = new LWM2MResourceInstance(LWM2MID.from(1), 65538);
-        assertEquals(1, instance.getId().intValue() );
-        assertTrue(instance.hasValue() );
-        assertEquals ("65538", instance.getStringValue() );
-        assertArrayEquals(new byte[] { 1, 0, 2 }, instance.getValue() );
+        assertEquals(1, instance.getId().intValue());
+        assertTrue(instance.hasValue());
+        assertEquals("65538", instance.getStringValue());
+        assertArrayEquals(new byte[]{1, 0, 2}, instance.getValue());
     }
-    
+
     @Test
     public void createBy4ByteInteger() throws Exception {
         LWM2MResourceInstance instance = new LWM2MResourceInstance(LWM2MID.from(1), 16777730);
-        assertEquals(1, instance.getId().intValue() );
-        assertTrue(instance.hasValue() );
-        assertEquals ("16777730", instance.getStringValue() );
-        assertArrayEquals(new byte[] { 1, 0, 2, 2 }, instance.getValue() );
+        assertEquals(1, instance.getId().intValue());
+        assertTrue(instance.hasValue());
+        assertEquals("16777730", instance.getStringValue());
+        assertArrayEquals(new byte[]{1, 0, 2, 2}, instance.getValue());
     }
-    
+
     @Test
     public void createByOpaque() throws Exception {
-        byte[] opaque = "opaque".getBytes();
+        byte[] opaque = Utf8Bytes.of("opaque");
         LWM2MResourceInstance instance = new LWM2MResourceInstance(LWM2MID.from(1), opaque);
-        assertEquals(1, instance.getId().intValue() );
-        assertTrue(instance.hasValue() );
-        assertArrayEquals(opaque, instance.getValue() );
+        assertEquals(1, instance.getId().intValue());
+        assertTrue(instance.hasValue());
+        assertArrayEquals(opaque, instance.getValue());
     }
-    
+
     @SuppressWarnings("unused")
-    @Test(expected=NullPointerException.class)
+    @Test(expected = NullPointerException.class)
     public void createWithNullID() throws Exception {
         new LWM2MResourceInstance(null);
     }
-    
+
     @SuppressWarnings("unused")
-    @Test(expected=IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void createWithNullValue() throws Exception {
         new LWM2MResourceInstance(LWM2MID.from(1), (byte[]) null);
     }
-    
+
     @SuppressWarnings("unused")
-    @Test(expected=IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void createWithEmptyValue() throws Exception {
         new LWM2MResourceInstance(LWM2MID.from(1), new byte[0]);
     }
-    
+
     @Test
     public void createWithNegativeID() throws Exception {
         LWM2MResourceInstance instance = new LWM2MResourceInstance(LWM2MID.create(), 42);
-        assertEquals(-1, instance.getId().intValue() );
+        assertEquals(-1, instance.getId().intValue());
     }
 
 }
--- src/test/java/com/arm/mbed/commons/lwm2m/json/JsonDeserializerTest.java
+++ src/test/java/com/arm/mbed/commons/lwm2m/json/JsonDeserializerTest.java
@@ -1,14 +1,13 @@
 /**
  * Copyright (C) 2011-2014 ARM Limited. All rights reserved.
  */
-
 package com.arm.mbed.commons.lwm2m.json;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 import com.arm.mbed.commons.lwm2m.LWM2MResource;
 import com.arm.mbed.commons.lwm2m.LWM2MResourceType;
-import com.arm.mbed.commons.lwm2m.json.JsonDeserializer;
+import com.arm.mbed.commons.string.Utf8Bytes;
 import java.io.InputStream;
 import java.util.List;
 import org.apache.commons.io.IOUtils;
@@ -23,14 +22,14 @@ public class JsonDeserializerTest {
     private static final String DEVICE_OBJECT_JSON = "device-object.json";
     private static final String NOTIFICATION_JSON = "notification.json";
     private static final String CUSTOM_OBJECT_JSON = "custom-object.json";
-    
+
     private JsonDeserializer deserializer;
 
     @Before
     public void setup() {
         deserializer = JsonDeserializer.create();
     }
-    
+
     @Test
     public void parseDeviceObject() throws Exception {
         InputStream input = JsonDeserializerTest.class.getResourceAsStream(DEVICE_OBJECT_JSON);
@@ -39,11 +38,11 @@ public class JsonDeserializerTest {
 
         assertThat(resources, hasSize(13));
         assertThat(resources.get(0).getType(), equalTo(LWM2MResourceType.STRING));
-        assertArrayEquals("Open Mobile Alliance".getBytes(), resources.get(0).getValue() );
+        assertArrayEquals(Utf8Bytes.of("Open Mobile Alliance"), resources.get(0).getValue());
         assertThat(resources.get(3).getType(), equalTo(LWM2MResourceType.STRING));
         assertThat(resources.get(4).getNestedInstances().get(0).getType(), equalTo(LWM2MResourceType.INTEGER));
     }
-    
+
     @Test
     public void parseNotification() throws Exception {
         InputStream input = JsonDeserializerTest.class.getResourceAsStream(NOTIFICATION_JSON);
@@ -55,7 +54,7 @@ public class JsonDeserializerTest {
         assertThat(resources.get(0).getNestedInstances(), hasSize(3));
         assertThat(resources.get(0).getNestedInstances().get(0).getType(), equalTo(LWM2MResourceType.FLOAT));
     }
-    
+
     @Test
     public void parseCustomObject() throws Exception {
         InputStream input = JsonDeserializerTest.class.getResourceAsStream(CUSTOM_OBJECT_JSON);
@@ -63,5 +62,5 @@ public class JsonDeserializerTest {
         List<LWM2MResource> resources = deserializer.deserialize(jsonString);
         System.out.println(resources);
     }
-    
+
 }
