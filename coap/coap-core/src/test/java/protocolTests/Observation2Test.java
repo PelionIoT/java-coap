3634b81fdcf426e686087d58405f450a76393936
Szymon Sasin <szymon.sasin@arm.com>
2015-11-10 16:47:01 +0200

    ONSP-986 fixed ack to a notification, not to include token

---
+++ coap/coap-core/src/test/java/protocolTests/Observation2Test.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2011-2015 ARM Limited. All rights reserved.
+ */
+package protocolTests;
+
+import static org.mbed.coap.server.CoapServerObserve.*;
+import static org.mockito.Mockito.*;
+import static org.testng.Assert.*;
+import static protocolTests.ObservationWithBlockTest.*;
+import static protocolTests.utils.CoapPacketBuilder.*;
+import java.net.InetSocketAddress;
+import org.mbed.coap.client.CoapClient;
+import org.mbed.coap.client.CoapClientBuilder;
+import org.mbed.coap.client.ObservationListener;
+import org.mbed.coap.packet.Code;
+import org.mbed.coap.server.CoapServer;
+import org.mbed.coap.server.CoapServerBuilder;
+import org.mbed.coap.server.MessageIdSupplierImpl;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import protocolTests.utils.CurrentThreadExecutor;
+import protocolTests.utils.TransportConnectorMock;
+
+/**
+ * Created by szymon
+ */
+public class Observation2Test {
+
+
+    private static final InetSocketAddress SERVER_ADDRESS = new InetSocketAddress("127.0.0.1", 5683);
+    private TransportConnectorMock transport;
+    private CoapClient client;
+    private ObservationListener observationListener;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        transport = new TransportConnectorMock();
+
+        CoapServer coapServer = CoapServerBuilder.newBuilder().transport(transport).midSupplier(new MessageIdSupplierImpl(0))
+                .executor(new CurrentThreadExecutor())
+                .observerIdGenerator(new SimpleObservationIDGenerator(0)).build();
+        coapServer.start();
+
+        client = CoapClientBuilder.clientFor(SERVER_ADDRESS, coapServer);
+
+        //establish observation relation
+        transport.when(newCoapPacket(1).get().uriPath("/path1").obs(0).token(1).build())
+                .then(newCoapPacket(1).ack(Code.C205_CONTENT).obs(0).token(1).payload("12345").build());
+
+        observationListener = mock(ObservationListener.class);
+        assertEquals("12345", client.resource("/path1").sync().observe(observationListener).getPayloadString());
+        reset(observationListener);
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+        client.close();
+    }
+
+    @Test
+    public void shouldReceiveEmptyAckAfterObservation() throws Exception {
+        //send observation
+        transport.receive(newCoapPacket(3).con(Code.C205_CONTENT).obs(1).token(1).payload("perse perse").build(), SERVER_ADDRESS);
+
+        //important, no token included in response
+        assertEquals(transport.getLastOutgoingMessage(), newCoapPacket(3).ack(null).build());
+        verify(observationListener).onObservation(hasPayload("perse perse"));
+    }
+}
