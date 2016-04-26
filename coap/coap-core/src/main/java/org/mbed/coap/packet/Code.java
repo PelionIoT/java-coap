2bff6cdbed0c00c87ed44a902b82ed8627882268
Szymon Sasin <szymon.sasin@arm.com>
2016-04-26 13:19:04 +0300

    fixed status code mapping from CoAP to HTTP

--- coap/coap-core/src/main/java/org/mbed/coap/packet/Code.java
+++ coap/coap-core/src/main/java/org/mbed/coap/packet/Code.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2015 ARM Limited. All rights reserved.
+ * Copyright (C) 2011-2016 ARM Limited. All rights reserved.
  */
 package org.mbed.coap.packet;
 
@@ -8,44 +8,43 @@ package org.mbed.coap.packet;
  */
 public enum Code {
     //RFC 7252
+    //draft-ietf-core-http-mapping-08 - for HTTP mapping
 
-    C201_CREATED(2, 01),
-    C202_DELETED(2, 02),
-    C203_VALID(2, 03),
-    C204_CHANGED(2, 04),
-    C205_CONTENT(2, 05),
-    C400_BAD_REQUEST(4, 00),
-    C401_UNAUTHORIZED(4, 01),
-    C402_BAD_OPTION(4, 02),
-    C403_FORBIDDEN(4, 03),
-    C404_NOT_FOUND(4, 04),
-    C405_METHOD_NOT_ALLOWED(4, 05),
-    C406_NOT_ACCEPTABLE(4, 06),
-    C412_PRECONDITION_FAILED(4, 12),
-    C415_UNSUPPORTED_MEDIA_TYPE(4, 15),
-    C500_INTERNAL_SERVER_ERROR(5, 00),
-    C501_NOT_IMPLEMENTED(5, 01),
-    C502_BAD_GATEWAY(5, 02),
-    C503_SERVICE_UNAVAILABLE(5, 03),
-    C504_GATEWAY_TIMEOUT(5, 04),
-    C505_PROXYING_NOT_SUPPORTED(5, 05),
+    C201_CREATED(2, 01, 201),
+    C202_DELETED(2, 02, 200),
+    C203_VALID(2, 03, 200),
+    C204_CHANGED(2, 04, 200),
+    C205_CONTENT(2, 05, 200),
+    C400_BAD_REQUEST(4, 00, 400),
+    C401_UNAUTHORIZED(4, 01, 403),
+    C402_BAD_OPTION(4, 02, 400),
+    C403_FORBIDDEN(4, 03, 403),
+    C404_NOT_FOUND(4, 04, 404),
+    C405_METHOD_NOT_ALLOWED(4, 05, 400),
+    C406_NOT_ACCEPTABLE(4, 06, 406),
+    C412_PRECONDITION_FAILED(4, 12, 412),
+    C415_UNSUPPORTED_MEDIA_TYPE(4, 15, 415),
+    C500_INTERNAL_SERVER_ERROR(5, 00, 500),
+    C501_NOT_IMPLEMENTED(5, 01, 501),
+    C502_BAD_GATEWAY(5, 02, 502),
+    C503_SERVICE_UNAVAILABLE(5, 03, 503),
+    C504_GATEWAY_TIMEOUT(5, 04, 504),
+    C505_PROXYING_NOT_SUPPORTED(5, 05, 502),
     //
     //draft-ietf-core-block-14
-    C231_CONTINUE(2, 31),
-    C408_REQUEST_ENTITY_INCOMPLETE(4, 8),
-    C413_REQUEST_ENTITY_TOO_LARGE(4, 13);
+    C231_CONTINUE(2, 31, 500),
+    C408_REQUEST_ENTITY_INCOMPLETE(4, 8, 500),
+    C413_REQUEST_ENTITY_TOO_LARGE(4, 13, 413);
     private final int coapCode;
+    private final int httpStatus;
 
-    private Code(int codeClass, int codeDetail) {
+    Code(int codeClass, int codeDetail, int httpStatus) {
         this.coapCode = (codeClass << 5) + codeDetail;
+        this.httpStatus = httpStatus;
     }
 
     public int getHttpCode() {
-        try {
-            return Integer.parseInt(name().substring(1, 4));
-        } catch (NumberFormatException numberFormatException) {
-            return 0;
-        }
+        return httpStatus;
     }
 
     public int getCoapCode() {
