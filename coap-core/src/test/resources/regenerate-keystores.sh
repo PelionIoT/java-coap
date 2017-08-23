db4e0d14a5d3b5233ea315b361735a78c92dcccc
Szymon Sasin <szymon.sasin@arm.com>
2017-08-23 10:50:22 +0300

    regenerated test keystores (expired certificates)

--- coap-core/src/test/resources/regenerate-keystores.sh
+++ coap-core/src/test/resources/regenerate-keystores.sh
@@ -9,7 +9,7 @@ mkdir -p target
 keytool $SRV_KS -genkey -keyalg EC -keysize 256  -alias server-ca -validity 720 -keypass secret -dname "CN=server-ca" -ext bc=ca:true
 keytool $SRV_KS -genkeypair -keyalg EC -keysize 256  -alias server -validity 720  -dname "CN=server" -keypass secret
 keytool $SRV_KS -certreq -alias server -file target/server.csr
-keytool $SRV_KS -gencert -alias server-ca -infile target/server.csr -outfile target/server.cer
+keytool $SRV_KS -gencert -alias server-ca -infile target/server.csr -outfile target/server.cer -validity 720
 keytool $SRV_KS -importcert -file target/server.cer -alias server
 keytool $SRV_KS -export -alias server-ca -file target/server-ca.cer
 
@@ -18,7 +18,7 @@ keytool $SRV_KS -export -alias server-ca -file target/server-ca.cer
 keytool $CLI_KS -genkey -keyalg EC -keysize 256  -alias client-ca -validity 720 -keypass secret -dname "CN=client-ca" -ext bc=ca:true
 keytool $CLI_KS -genkeypair -keyalg EC -keysize 256  -alias client -validity 720  -dname "CN=client" -keypass secret
 keytool $CLI_KS -certreq -alias client -file target/client.csr
-keytool $CLI_KS -gencert -alias client-ca -infile target/client.csr -outfile target/client.cer
+keytool $CLI_KS -gencert -alias client-ca -infile target/client.csr -outfile target/client.cer -validity 720
 keytool $CLI_KS -importcert -file target/client.cer -alias client
 keytool $CLI_KS -export -alias client-ca -file target/client-ca.cer
 
